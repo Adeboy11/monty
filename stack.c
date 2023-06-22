c745b9042743b04cf8fedf9d748ee9ff9b75fefb
olakunle-remi <abbeypentious@gmail.com>
2023-06-22 12:38:40 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO patner project: stack.c

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * stack - display the top
+ * @head: stack head
+ * @index: line no
+ * Return: none
+*/
+void stack(stack_t **head, unsigned int index)
+{
+	(void)head;
+	(void)index;
+	buff.lifo = 0;
+}
