c53466ad75abd80de5ff8b29266671360b7a8e5b
olakunle-remi <abbeypentious@gmail.com>
2023-06-22 12:44:17 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO partner project: rotr.c

---
+++ rotr.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+  *rotr- rotates the stack to the bottom
+  *@head: stack head
+  *@index: line no
+  *Return: none
+ */
+void rotr(stack_t **head, __attribute__((unused)) unsigned int index)
+{
+	stack_t *cmp;
+
+	cmp = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (cmp->next)
+	{
+		cmp = cmp->next;
+	}
+	cmp->next = *head;
+	cmp->prev->next = NULL;
+	cmp->prev = NULL;
+	(*head)->prev = cmp;
+	(*head) = cmp;
+}
