49aaab19d60bf28cfce5e4e5517f8c5247f26e9f
olakunle-remi <abbeypentious@gmail.com>
2023-06-22 13:03:17 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO partner project : swap.c

---
+++ swap.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * swap - swap top two elements of the stack.
+ * @head: stack head
+ * @index: line no
+ * Return: none
+*/
+void swap(stack_t **head, unsigned int index)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", index);
+		fclose(buff.file);
+		free(buff.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
