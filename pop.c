732a3b3bc68c26cfcf0e02f595ddc46e7e2eba8f
olakunle-remi <abbeypentious@gmail.com>
2023-06-22 14:02:19 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO partner project: pop.c

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * pop - display the top
+ * @head: stack head
+ * @index: line no
+ * Return: none
+*/
+void pop(stack_t **head, unsigned int index)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", index);
+		fclose(buff.file);
+		free(buff.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
