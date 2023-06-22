c0251dd97e7d3a95910ef51a13a144abba9eec45
olakunle-remi <abbeypentious@gmail.com>
2023-06-22 12:51:47 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO partner project : sub-1.c

---
+++ sub-1.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+  *sub- sustrate from the stack
+  *@head: stack head
+  *@index: line no
+  *Return: no return
+ */
+void sub(stack_t **head, unsigned int index)
+{
+	stack_t *aux;
+	int mpx, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", index);
+		fclose(buff.file);
+		free(buff.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = *head;
+	mpx = aux->next->n - aux->n;
+	aux->next->n = mpx;
+	*head = aux->next;
+	free(aux);
+}
